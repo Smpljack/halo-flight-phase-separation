c615a28ea4290da854b20f0f253f66823942c59e
Marc Prange <marc.prange@gmx.de>
2020-06-03 17:16:18 +0200

    Update ResearchFlightSegment class.

--- ResearchFlightSegment.py
+++ ResearchFlightSegment.py
@@ -1,5 +1,5 @@
 class ResearchFlightSegment:
-    def __init__(self, kinds, name, good_dropsondes, irregularities, segment_id, start, end):
+    def __init__(self, kinds, name, irregularities, segment_id, start, end, good_dropsondes=None):
         """
         Initialize the attributes of a research flight segment.
         :param kind: The general kind (or type) of the research flight segment.
@@ -11,6 +11,10 @@ class ResearchFlightSegment:
         self.name = name
         self.start = start
         self.end = end
+        if 'circle' in self.kinds or 'circling' in self.kinds:
+            self.good_dropsondes = good_dropsondes
+        self.irregularities = irregularities
+        self.segment_id = segment_id
 
     def to_dictionary(self):
         """
@@ -18,9 +22,13 @@ class ResearchFlightSegment:
         :return: Dictionary containing the attributes of the research flight segment.
         """
         rf_segment_dict = {
-            'kind': self.kind,
+            'kinds': self.kinds,
             'name': self.name,
+            'irregularities': self.irregularities,
+            'segment_id': self.segment_id,
             'start': self.start,
             'end': self.end,
         }
+        if 'circle' in self.kinds or 'circling' in self.kinds:
+            rf_segment_dict['good_dropsondes'] = self.good_dropsondes
         return rf_segment_dict
