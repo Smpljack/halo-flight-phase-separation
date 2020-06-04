8cb0ea900901c5bdf156a2ea04b16041c89ede8d
Tobias Kölling <tobi@die70.de>
2020-06-04 17:29:05 +0200

    added altitude zoom to lidar_leg report

--- report.py
+++ report.py
@@ -137,7 +137,7 @@ SPECIAL_PLOTS = {
     "circle": [circle_detail_plot],
     "straight_leg": [straight_leg_detail_plot],
     "radar_calibration_wiggle": [zoom_on("roll", "deg")],
-    "lidar_leg": [timeline_of("altitude", "m")],
+    "lidar_leg": [timeline_of("altitude", "m"), zoom_on("altitude", "m")],
 }
 
 def plots_for_kinds(kinds):
