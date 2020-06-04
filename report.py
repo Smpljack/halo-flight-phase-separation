90c6b8daed7a075f303b08e6d852e2a33bc6a22b
Tobias Kölling <tobi@die70.de>
2020-06-04 19:04:54 +0200

    [refactor] added color variables for reports

--- report.py
+++ report.py
@@ -10,6 +10,11 @@ from base64 import b64encode
 
 border_time = np.timedelta64(3, "m")
 
+color_before = "C2"
+color_at = "C0"
+color_after = "C3"
+color_sonde = "C1"
+
 env = Environment(
     loader=FileSystemLoader(os.path.join(os.path.dirname(__file__), 'templates')),
     autoescape=select_autoescape(['html', 'xml'])
@@ -40,19 +45,19 @@ def default_segment_plot(seg, sonde_track, seg_before, seg_after):
     pitch_ax = fig.add_subplot(spec[1, 2], sharex=roll_ax)
     yaw_ax = fig.add_subplot(spec[2, 2], sharex=roll_ax)
 
-    overview_ax.plot(seg.lon, seg.lat, zorder=10)
-    overview_ax.plot(seg_before.lon, seg_before.lat, color="C3", alpha=.3, zorder=0)
-    overview_ax.plot(seg_after.lon, seg_after.lat, color="C3", alpha=.3, zorder=0)
-    overview_ax.scatter(sonde_track.lon, sonde_track.lat, color="C1", zorder=5)
+    overview_ax.plot(seg.lon, seg.lat, color=color_at, zorder=10)
+    overview_ax.plot(seg_before.lon, seg_before.lat, color=color_before, alpha=.3, zorder=0)
+    overview_ax.plot(seg_after.lon, seg_after.lat, color=color_after, alpha=.3, zorder=0)
+    overview_ax.scatter(sonde_track.lon, sonde_track.lat, color=color_sonde, zorder=5)
 
     overview_ax.set_title("segment overview")
     overview_ax.set_xlabel("longitude [deg]")
     overview_ax.set_ylabel("latitude [deg]")
 
     for ax, var in [(roll_ax, "roll"), (pitch_ax, "pitch"), (yaw_ax, "heading")]:
-        seg[var].plot(ax=ax, zorder=10)
-        seg_before[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
-        seg_after[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
+        seg[var].plot(ax=ax, color=color_at, zorder=10)
+        seg_before[var].plot(ax=ax, color=color_before, alpha=.3, zorder=0)
+        seg_after[var].plot(ax=ax, color=color_after, alpha=.3, zorder=0)
         ax.set_title(var)
         ax.set_ylabel("deg")
 
@@ -64,11 +69,10 @@ def default_segment_plot(seg, sonde_track, seg_before, seg_after):
 
 def circle_detail_plot(seg, sonde_track, seg_before, seg_after):
     fig, zoom_ax = plt.subplots(1, figsize=(4,4), constrained_layout=True)
-    zoom_ax.plot(seg.lon, seg.lat, "o-", zorder=10)
-    zoom_ax.plot(seg_before.lon, seg_before.lat, "x-", color="C3", alpha=.3, zorder=0)
-    zoom_ax.plot(seg_after.lon, seg_after.lat, "x-", color="C3", alpha=.3, zorder=0)
-
-    zoom_ax.scatter(sonde_track.lon, sonde_track.lat, color="C1", zorder=5)
+    zoom_ax.plot(seg.lon, seg.lat, "o-", color=color_at, zorder=10)
+    zoom_ax.plot(seg_before.lon, seg_before.lat, "x-", color=color_before, alpha=.3, zorder=0)
+    zoom_ax.plot(seg_after.lon, seg_after.lat, "x-", color=color_after, alpha=.3, zorder=0)
+    zoom_ax.scatter(sonde_track.lon, sonde_track.lat, color=color_sonde, zorder=5)
     lat_lims, lon_lims = start_end_lims(seg)
     zoom_ax.set_xlim(*lon_lims)
     zoom_ax.set_ylim(*lat_lims)
@@ -87,10 +91,10 @@ def straight_leg_detail_plot(seg, sonde_track, seg_before, seg_after):
 
     for ax, lat, lon, name in [(start_ax, start_lat, start_lon, "start"),
                                (end_ax, end_lat, end_lon, "end")]:
-        ax.plot(seg.lon, seg.lat, "o-", zorder=10)
-        ax.plot(seg_before.lon, seg_before.lat, "x-", color="C3", alpha=.3, zorder=0)
-        ax.plot(seg_after.lon, seg_after.lat, "x-", color="C3", alpha=.3, zorder=0)
-        ax.scatter(sonde_track.lon, sonde_track.lat, color="C1", zorder=5)
+        ax.plot(seg.lon, seg.lat, "o-", color=color_at, zorder=10)
+        ax.plot(seg_before.lon, seg_before.lat, "x-", color=color_before, alpha=.3, zorder=0)
+        ax.plot(seg_after.lon, seg_after.lat, "x-", color=color_after, alpha=.3, zorder=0)
+        ax.scatter(sonde_track.lon, sonde_track.lat, color=color_sonde, zorder=5)
 
         ax.set_xlim(lon - .1, lon + .1)
         ax.set_ylim(lat - .1, lat + .1)
@@ -109,9 +113,9 @@ def zoom_on(var, unit):
 
         for ax, t, name in [(start_ax, seg.time.data[0], "start"),
                       (end_ax, seg.time.data[-1], "end")]:
-            seg[var].plot(ax=ax, zorder=10)
-            seg_before[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
-            seg_after[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
+            seg[var].plot(ax=ax, color=color_at, zorder=10)
+            seg_before[var].plot(ax=ax, color=color_before, alpha=.3, zorder=0)
+            seg_after[var].plot(ax=ax, color=color_after, alpha=.3, zorder=0)
             ax.set_title("zoom on {}".format(name))
             ax.set_ylabel("{} [{}]".format(var, unit))
 
@@ -124,9 +128,9 @@ def timeline_of(var, unit):
     def plot(seg, sonde_track, seg_before, seg_after):
         fig, ax  = plt.subplots(1, 1, figsize=(8,3), constrained_layout=True)
 
-        seg[var].plot(ax=ax, zorder=10)
-        seg_before[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
-        seg_after[var].plot(ax=ax, color="C3", alpha=.3, zorder=0)
+        seg[var].plot(ax=ax, color=color_at, zorder=10)
+        seg_before[var].plot(ax=ax, color=color_before, alpha=.3, zorder=0)
+        seg_after[var].plot(ax=ax, color=color_after, alpha=.3, zorder=0)
         ax.set_title(var)
         ax.set_ylabel("{} [{}]".format(var, unit))
 
